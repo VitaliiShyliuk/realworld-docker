393048c9eb36a700c381f92dc9d89ab5b63508b0
Vitalii Shyliuk <v.m.shyliuk@gmail.com>
2022-12-26 02:04:07 +0200

    connect api db

---
+++ api/src/configuration/index.js
@@ -0,0 +1,3 @@
+module.exports.db = process.env.MONGO_URL;
+module.exports.port = process.env.PORT;
+module.exports.host = process.env.HOST;
--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const { port, host } = require("./configuration");
+const { connectDb } = require("./helpers/db");
 const app = express();
-const PORT = 3000;
 
 app.get("/test", (req, res) => {
-  res.send("Api | test");
+  res.send(`Api | test ${port} ${host}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Api | Started on port: ${PORT}`);
-});
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Api | Started on port: ${port}`);
+    console.log(`Api | Host: ${host}`);
+  });
+};
+
+connectDb()
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
